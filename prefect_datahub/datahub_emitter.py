29ebd0e982e3848475f4ece637de4ed2537aab70
shubhamjagtap639 <shubhamjagtap639@gmail.com>
2023-06-13 11:49:31 +0530

    Code formatted

--- prefect_datahub/datahub_emitter.py
+++ prefect_datahub/datahub_emitter.py
@@ -25,7 +25,53 @@ from prefect.context import FlowRunContext, TaskRunContext
 from prefect.settings import PREFECT_API_URL
 from pydantic import Field
 
-from prefect_datahub import constants
+ORCHESTRATOR = "prefect"
+
+# Flow and task common constants
+VERSION = "version"
+RETRIES = "retries"
+TIMEOUT_SECONDS = "timeout_seconds"
+LOG_PRINTS = "log_prints"
+ON_COMPLETION = "on_completion"
+ON_FAILURE = "on_failure"
+
+# Flow constants
+FLOW_RUN_NAME = "flow_run_name"
+TASK_RUNNER = "task_runner"
+PERSIST_RESULT = "persist_result"
+ON_CANCELLATION = "on_cancellation"
+ON_CRASHED = "on_crashed"
+
+# Task constants
+CACHE_EXPIRATION = "cache_expiration"
+TASK_RUN_NAME = "task_run_name"
+REFRESH_CACHE = "refresh_cache"
+TASK_KEY = "task_key"
+
+# Flow run and task run common constants
+ID = "id"
+CREATED = "created"
+UPDATED = "updated"
+TAGS = "tags"
+ESTIMATED_RUN_TIME = "estimated_run_time"
+START_TIME = "start_time"
+END_TIME = "end_time"
+TOTAL_RUN_TIME = "total_run_time"
+NEXT_SCHEDULED_START_TIME = "next_scheduled_start_time"
+
+# Fask run constants
+CREATED_BY = "created_by"
+AUTO_SCHEDULED = "auto_scheduled"
+
+# Task run constants
+FLOW_RUN_ID = "flow_run_id"
+RUN_COUNT = "run_count"
+UPSTREAM_DEPENDENCIES = "upstream_dependencies"
+
+# States constants
+COMPLETE = "Completed"
+FAILED = "Failed"
+CANCELLED = "Cancelled"
 
 
 class WorkspaceKey(PlatformKey):
@@ -142,7 +188,7 @@ class DatahubEmitter(Block):
             The datajob entity.
         """
         dataflow_urn = DataFlowUrn.create_from_ids(
-            orchestrator=constants.ORCHESTRATOR,
+            orchestrator=ORCHESTRATOR,
             flow_id=flow_run_ctx.flow.name,
             env=self.env,
             platform_instance=self.platform_instance,
@@ -159,16 +205,16 @@ class DatahubEmitter(Block):
             job_property_bag: Dict[str, str] = {}
 
             allowed_task_keys = [
-                constants.VERSION,
-                constants.CACHE_EXPIRATION,
-                constants.TASK_RUN_NAME,
-                constants.RETRIES,
-                constants.TIMEOUT_SECONDS,
-                constants.LOG_PRINTS,
-                constants.REFRESH_CACHE,
-                constants.TASK_KEY,
-                constants.ON_COMPLETION,
-                constants.ON_FAILURE,
+                VERSION,
+                CACHE_EXPIRATION,
+                TASK_RUN_NAME,
+                RETRIES,
+                TIMEOUT_SECONDS,
+                LOG_PRINTS,
+                REFRESH_CACHE,
+                TASK_KEY,
+                ON_COMPLETION,
+                ON_FAILURE,
             ]
             for key in allowed_task_keys:
                 if (
@@ -200,7 +246,7 @@ class DatahubEmitter(Block):
             orchestration.get_client().read_flow(flow_id=flow_run_ctx.flow_run.flow_id)
         )
         dataflow = DataFlow(
-            orchestrator=constants.ORCHESTRATOR,
+            orchestrator=ORCHESTRATOR,
             id=flow_run_ctx.flow.name,
             cluster=self.env,
             env=self.env,
@@ -210,22 +256,22 @@ class DatahubEmitter(Block):
         dataflow.description = flow_run_ctx.flow.description
         dataflow.tags = flow.tags
         flow_property_bag: Dict[str, str] = {}
-        flow_property_bag[constants.ID] = str(flow.id)
-        flow_property_bag[constants.CREATED] = str(flow.created)
-        flow_property_bag[constants.UPDATED] = str(flow.updated)
+        flow_property_bag[ID] = str(flow.id)
+        flow_property_bag[CREATED] = str(flow.created)
+        flow_property_bag[UPDATED] = str(flow.updated)
 
         allowed_flow_keys = [
-            constants.VERSION,
-            constants.FLOW_RUN_NAME,
-            constants.RETRIES,
-            constants.TASK_RUNNER,
-            constants.TIMEOUT_SECONDS,
-            constants.PERSIST_RESULT,
-            constants.LOG_PRINTS,
-            constants.ON_COMPLETION,
-            constants.ON_FAILURE,
-            constants.ON_CANCELLATION,
-            constants.ON_CRASHED,
+            VERSION,
+            FLOW_RUN_NAME,
+            RETRIES,
+            TASK_RUNNER,
+            TIMEOUT_SECONDS,
+            PERSIST_RESULT,
+            LOG_PRINTS,
+            ON_COMPLETION,
+            ON_FAILURE,
+            ON_CANCELLATION,
+            ON_CRASHED,
         ]
         for key in allowed_flow_keys:
             if (
@@ -258,17 +304,17 @@ class DatahubEmitter(Block):
 
         dpi_property_bag: Dict[str, str] = {}
         allowed_flow_run_keys = [
-            constants.ID,
-            constants.CREATED,
-            constants.UPDATED,
-            constants.CREATED_BY,
-            constants.AUTO_SCHEDULED,
-            constants.ESTIMATED_RUN_TIME,
-            constants.START_TIME,
-            constants.TOTAL_RUN_TIME,
-            constants.NEXT_SCHEDULED_START_TIME,
-            constants.TAGS,
-            constants.RUN_COUNT,
+            ID,
+            CREATED,
+            UPDATED,
+            CREATED_BY,
+            AUTO_SCHEDULED,
+            ESTIMATED_RUN_TIME,
+            START_TIME,
+            TOTAL_RUN_TIME,
+            NEXT_SCHEDULED_START_TIME,
+            TAGS,
+            RUN_COUNT,
         ]
         for key in allowed_flow_run_keys:
             if hasattr(flow_run, key) and getattr(flow_run, key) is not None:
@@ -304,17 +350,17 @@ class DatahubEmitter(Block):
 
         dpi_property_bag: Dict[str, str] = {}
         allowed_task_run_keys = [
-            constants.ID,
-            constants.FLOW_RUN_ID,
-            constants.CREATED,
-            constants.UPDATED,
-            constants.ESTIMATED_RUN_TIME,
-            constants.START_TIME,
-            constants.END_TIME,
-            constants.TOTAL_RUN_TIME,
-            constants.NEXT_SCHEDULED_START_TIME,
-            constants.TAGS,
-            constants.RUN_COUNT,
+            ID,
+            FLOW_RUN_ID,
+            CREATED,
+            UPDATED,
+            ESTIMATED_RUN_TIME,
+            START_TIME,
+            END_TIME,
+            TOTAL_RUN_TIME,
+            NEXT_SCHEDULED_START_TIME,
+            TAGS,
+            RUN_COUNT,
         ]
         for key in allowed_task_run_keys:
             if hasattr(task_run, key) and getattr(task_run, key) is not None:
@@ -322,9 +368,9 @@ class DatahubEmitter(Block):
         dpi.properties.update(dpi_property_bag)
 
         state_result_map: Dict[str, str] = {}
-        state_result_map[constants.COMPLETE] = InstanceRunResult.SUCCESS
-        state_result_map[constants.FAILED] = InstanceRunResult.FAILURE
-        state_result_map[constants.CANCELLED] = InstanceRunResult.SKIPPED
+        state_result_map[COMPLETE] = InstanceRunResult.SUCCESS
+        state_result_map[FAILED] = InstanceRunResult.FAILURE
+        state_result_map[CANCELLED] = InstanceRunResult.SKIPPED
 
         if task_run.state_name in state_result_map:
             result = state_result_map[task_run.state_name]
@@ -344,7 +390,7 @@ class DatahubEmitter(Block):
             emitter=self.emitter,
             end_timestamp_millis=int(task_run.end_time.timestamp() * 1000),
             result=result,
-            result_type=constants.ORCHESTRATOR,
+            result_type=ORCHESTRATOR,
         )
 
     def _emit_workspaces(self) -> Optional[str]:
@@ -376,7 +422,7 @@ class DatahubEmitter(Block):
             if str(workspace.workspace_id) == current_workspace_id:
                 container_key = WorkspaceKey(
                     workspace_name=workspace.workspace_name,
-                    platform=constants.ORCHESTRATOR,
+                    platform=ORCHESTRATOR,
                     instance=self.platform_instance,
                     env=self.env,
                 )
@@ -499,9 +545,7 @@ class DatahubEmitter(Block):
             for prefect_future in flow_run_ctx.task_run_futures
         }
         for node in graph_json:
-            task_run = asyncio.run(
-                orchestration.get_client().read_task_run(node[constants.ID])
-            )
+            task_run = asyncio.run(orchestration.get_client().read_task_run(node[ID]))
             # Emit task
             datajob_urn = DataJobUrn.create_from_ids(
                 data_flow_urn=str(dataflow.urn),
@@ -514,10 +558,10 @@ class DatahubEmitter(Block):
                     flow_run_ctx=flow_run_ctx, task_key=task_run.task_key
                 )
             # Add upstrem urns
-            for each in node[constants.UPSTREAM_DEPENDENCIES]:
+            for each in node[UPSTREAM_DEPENDENCIES]:
                 upstream_task_urn = DataJobUrn.create_from_ids(
                     data_flow_urn=str(dataflow.urn),
-                    job_id=task_run_key_map[each[constants.ID]],
+                    job_id=task_run_key_map[each[ID]],
                 )
                 datajob.upstream_urns.extend([upstream_task_urn])
             datajob.emit(self.emitter)
